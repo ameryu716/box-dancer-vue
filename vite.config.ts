f061ed052c5bda39ccfcb8b2fc062cec8dff8150
Swallow2022 <woodguiter28@gmail.com>
2026-06-07 14:24:31 +0900

    pnpm&cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,11 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
+export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
-    console.log(command);
-    console.log(mode);
-    if (command === "serve") {
-        return {
-            plugins: [vue()],
-        };
-    } else {
-        return {
-            plugins: [vue()],
-            base: env.BUILD_BASE_URL,
-        };
-    }
+
+    return {
+        plugins: [vue()],
+        base: command === "build" ? env.BUILD_BASE_URL || "/" : "/",
+    };
 });
